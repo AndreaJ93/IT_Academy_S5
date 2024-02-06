196a4930a8270da00b47315822f05ce1b9f5d2c0
Andrea Jordan <andreajt.1993@gmail.com>
2024-02-06 12:55:58 +0100

    Onboarding digital amb animació

--- src/components/Indicator.jsx
+++ src/components/Indicator.jsx
@@ -1,46 +1,54 @@
-import styled, { keyframes } from "styled-components"
+import styled, { keyframes } from "styled-components";
 
 //Animación de los indicadores:
 const IndicatorAnimation = keyframes`
  0% { width: 6px; background-color: #D7DBDE;}
  100% { width: 15px; background-color: black;}
-`
+`;
 const ButtonIndicator = styled.div`
-width: 6px;
-height: 6px;
-border-radius: 50%;
-background-color: #D7DBDE;
-display: inline-block;
-margin: 3px;
-cursor: pointer;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background-color: #d7dbde;
+  display: inline-block;
+  margin: 3px;
+  cursor: pointer;
 `;
 
 const ButtonSelected = styled.div`
-height: 6px;
-border-radius: 5px;
-background-color: black;
-display: inline-block;
-margin: 3px;
-cursor: pointer;
-animation-name: ${IndicatorAnimation};
-animation-duration: 1s;
-animation-fill-mode: forwards
+  height: 6px;
+  border-radius: 5px;
+  background-color: black;
+  display: inline-block;
+  margin: 3px;
+  cursor: pointer;
+  animation-name: ${IndicatorAnimation};
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
 `;
 
-
-
-function Indicator ({tutorialData, step, setStep}) {
-
-    return (
+function Indicator({ tutorialData, step, setStep, translateX, setTranslateX }) {
+  return (
     <>
-    {tutorialData.map ((data, index) => {
-        return (
-        index === step 
-        ? <ButtonSelected key = {index} onClick={() => setStep(index)}></ButtonSelected> 
-        : <ButtonIndicator key = {index} onClick={() => setStep(index)}></ButtonIndicator>
-        )
-    })}
+      {tutorialData.map((data, index) => {
+        return index === step ? (
+          <ButtonSelected
+            key={index}
+            onClick={() => {
+              setStep(index);
+            }}
+          ></ButtonSelected>
+        ) : (
+          <ButtonIndicator
+            key={index}
+            onClick={() => {
+              setTranslateX(translateX + (step - index) * 360);
+              setStep(index);
+            }}
+          ></ButtonIndicator>
+        );
+      })}
     </>
-    )
+  );
 }
-export default Indicator
+export default Indicator;
